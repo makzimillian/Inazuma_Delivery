cd467baa6394fbfcd3058e20be438b7337ace77c
makzimillian <maxmcgowan@gmail.com>
2014-10-16 14:37:21 +0800

    New App filing system - layoustill requiring work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    # /?admin=true
+
+    if params[:admin] == "true"
+      render 'index'
+    else
+      render 'under_construction', layout: false
+    end
   end
 end
