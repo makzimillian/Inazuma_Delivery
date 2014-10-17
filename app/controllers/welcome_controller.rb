bb747020abd7debbe707bdc3e56215110c192030
makzimillian <maxmcgowan@gmail.com>
2014-10-17 15:32:02 +0800

    updates, pics and database links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,22 @@
 class WelcomeController < ApplicationController
-  def index
-    # /?admin=true
 
-    if params[:admin] == "true"
-      render 'index'
-    else
-      render 'under_construction', layout: false
-    end
+  def index
+
+   # @delivery_time = "..." #Time.now
+
+   @item1 = FoodItem.where(cuisine: "Asian").sample
+   @item2 = FoodItem.where(cuisine: "Western").sample
+   @item3 = FoodItem.where(cuisine: "Adventurous").sample
+
   end
 end
+
+
+
+# class WelcomeController < ApplicationController
+#   def index
+#     @western_item = FoodItem.where(cuisine: "Western").sample
+#     @asian_item = FoodItem.where(cuisine: "Asian").sample
+#     @adventurous_item = FoodItem.where(cuisine: "Adventurous").sample
+#   end
+# end
