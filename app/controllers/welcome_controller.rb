a4e9167b82ee8709bf4e563dd203995e3a21d136
makzimillian <maxmcgowan@gmail.com>
2014-10-17 15:53:25 +0800

    more updates, routes.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,22 +1,11 @@
 class WelcomeController < ApplicationController
-
   def index
-
-   # @delivery_time = "..." #Time.now
-
-   @item1 = FoodItem.where(cuisine: "Asian").sample
-   @item2 = FoodItem.where(cuisine: "Western").sample
-   @item3 = FoodItem.where(cuisine: "Adventurous").sample
-
+    @western_item = FoodItem.where(cuisine: "Western").sample
+    @asian_item = FoodItem.where(cuisine: "Asian").sample
+    @adventurous_item = FoodItem.where(cuisine: "Adventurous").sample
   end
 end
 
-
-
-# class WelcomeController < ApplicationController
-#   def index
-#     @western_item = FoodItem.where(cuisine: "Western").sample
-#     @asian_item = FoodItem.where(cuisine: "Asian").sample
-#     @adventurous_item = FoodItem.where(cuisine: "Adventurous").sample
-#   end
-# end
+   # @item1 = FoodItem.where(cuisine: "Asian").sample
+   # @item2 = FoodItem.where(cuisine: "Western").sample
+   # @item3 = FoodItem.where(cuisine: "Adventurous").sample
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'food_item/show:id' => 'food_item#show'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
